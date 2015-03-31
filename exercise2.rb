f03e686d391f5df5838ae8487b905c53877da8d3
Jordan Winick <jwin47@gmail.com>
2015-03-31 15:04:40 -0400

    exercise 2 complete

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+#Calculate a good tip
+puts 55*0.2
+
+#add string and int
+puts "hi"+2.to_s
+
+#multiply numbers
+puts "45628x7839=#{45628*7839}"
+
+#boolean threesome
+puts (true && false) || (false && true) || !(false && false)
